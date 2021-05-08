f0f5ef84bb1d0483961a431b03a7c84219c6efd2
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-05-08 09:48:49 +0300

    add Program.cs, Tuple.cs

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TupleClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string personFullName = $"{firstTokens[0]} {firstTokens[1]}";
+            string personAddress = firstTokens[2];
+
+            Tuple<string, string> personInfoTupleOne = new Tuple<string, string>(personFullName, personAddress);
+            //Console.WriteLine(personInfoTupleOne);
+
+            string[] secTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string personName = $"{secTokens[0]}";
+            int beerData = int.Parse($"{secTokens[1]}");
+
+            Tuple<string, int> personInfoTupleSec = new Tuple<string, int>(personName, beerData);
+
+            string[] thirdTokens = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int numOne = int.Parse($"{thirdTokens[0]}");
+            double numTwo = double.Parse($"{thirdTokens[1]}");
+
+            Tuple<int, double> numsInfoTupleThird = new Tuple<int, double>(numOne, numTwo);
+
+            Console.WriteLine(personInfoTupleOne);
+            Console.WriteLine(personInfoTupleSec);
+            Console.WriteLine(numsInfoTupleThird);
+        }
+    }
+}
---
+++ Tuple.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TupleClass
+{
+    class Tuple<TFirst, TSec>  // Tuple<string, int>
+    {
+        public TFirst FirstItem { get; set; }
+        public TSec SecItem { get; set; }
+
+        public Tuple(TFirst firstItem, TSec secondItem)
+        {
+            this.FirstItem = firstItem;
+            this.SecItem = secondItem;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecItem}";
+        }
+    }
+}
